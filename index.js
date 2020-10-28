5df99fec761ee04539817c4f36c09a16a5c827a0
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-10-28 17:02:27 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// multi-dimensional array or nested array
+// Only change code below this line
+var myArray = [["Sarah", 30], ["Henry", 6]];
+
+
+
+// Create a variable called myData and set it to equal the first value of myArray using bracket notation.
+// Setup
+var myArray = [50,60,70];
+// Only change code below this line
+var myData = myArray[0]
+
+
+// Modify the data stored at index 0 of myArray to a value of 45.
+// Unlike strings, the entries of arrays are mutable and can be changed freely.
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line
+myArray[0] = 45
+
+
+//Push ["dog", 3] onto the end of the myArray variable.
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(["dog", 3])
+
+
+// shift() removes 1st element in array
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+var removedFromMyArray = myArray.shift();
